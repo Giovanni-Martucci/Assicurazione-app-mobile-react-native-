8be5305d5847c9f0af5a6e884b3a6b52ad0f4835
Giovanni-Martucci <40646559+Giovanni-Martucci@users.noreply.github.com>
2022-12-25 20:55:35 +0100

    Fix

--- assicurazione-client/App.js
+++ assicurazione-client/App.js
@@ -8,12 +8,12 @@ import Details from "./components/Details";
 import * as firebase from "firebase";
 
 var config = {
-  apiKey: "AIzaSyAOQWcSIhSad9BvEM3Aj-psdlEXpJMQUP4",
-  authDomain: "assicurazione-6d1c4.firebaseapp.com",
-  databaseURL: "https://assicurazione-6d1c4.firebaseio.com",
-  projectId: "assicurazione-6d1c4",
-  storageBucket: "assicurazione-6d1c4.appspot.com",
-  messagingSenderId: "597789520191"
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: ""
 };
 !firebase.apps.length ? firebase.initializeApp(config) : null;
 
